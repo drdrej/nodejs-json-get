b189ed7707656677cf88d11c58bc38064e5955a8
Andreas Siebert <andrej.siebert@gmail.com>
2014-02-09 20:26:01 +0100

    add trimming

--- impl/asText.js
+++ impl/asText.js
@@ -4,19 +4,30 @@
  * @param value simple value or array of values. will be
  * converted to a simple text-string of text values.
  *
- * @returns {string}
+ * @param options
+ *
+ * @returns {string}  trimmed string, defaultValue or undefined.
  *
  * @author A. Siebert, ask@touchableheroes.com
  */
 exports.asText = function (value, options) {
     var _ = require('underscore');
 
+    var trimmed = function( value ) {
+        if( options && options.trim ) {
+            var S = require( "string" );
+            return S(value).trim();
+        }
+
+        return value;
+    };
+
     if (!value && value != false) {
         return options.defaultVal;
     }
 
     if (_.isString(value)) {
-        return value;
+        return trimmed(value);
     }
 
     if (_.isArray(value)) {
@@ -26,12 +37,12 @@ exports.asText = function (value, options) {
             rval += entry ? entry : "";
         });
 
-        return rval;
+        return trimmed(rval);
     }
 
     console.log("-- couldn't convert value to string: %j", value);
     if (options && options.defaultVal)
         return options.defaultVal;
 
-    return
+    return;
 };
